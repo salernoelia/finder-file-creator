36ecf5b2fc1349deafdc06aea9658b2e13e4a879
Elia Salerno <hello@eliasalerno.ch>
2026-04-10 14:19:07 +0200

    Initial Commit

---
+++ finder-file-creator/finder_file_creatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  finder_file_creatorApp.swift
+//  finder-file-creator
+//
+//  Created by Elia Salerno on 10.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct finder_file_creatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
